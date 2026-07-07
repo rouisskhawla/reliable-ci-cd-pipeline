2f62b2e7b0068497fbc7a3dd9d8cf3687bca7925
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-07-07 15:22:36 +0100

    replace deprecated @MockBean

--- services/authors-service/src/test/java/com/devops/microservices/author/controller/AuthorControllerTest.java
+++ services/authors-service/src/test/java/com/devops/microservices/author/controller/AuthorControllerTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
@@ -25,7 +25,7 @@ class AuthorControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AuthorService authorService;
 
     @Autowired
